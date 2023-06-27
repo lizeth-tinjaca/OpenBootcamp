05eead46b356805dceec458fe8a5ccf8bd8aaa6f
Lizeth Tinjaca <davidylizeth1411@gmail.com>
2023-06-27 17:34:13 -0500

    Ejercicio de tipos primitivos

---
+++ Tipos primitivos.js
@@ -0,0 +1,21 @@
+//tipos primitivos 
+
+//Number
+5 ; 
+
+//string 
+'Hola Mundo';
+
+//Boolean
+true;
+false;
+
+//Null
+null;
+undefined;
+//Null, undefined, false,===> Todas son falsy
+if (0) {
+  console.log('Cumple')
+} else {
+    console.log('No cumple')
+}
