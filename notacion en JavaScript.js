7c9f2b1556a38935586ace80970969d4df2b75d5
Lizeth Tinjaca <davidylizeth1411@gmail.com>
2023-06-29 15:37:26 -0500

    curso de notacion de Javascript

---
+++ notacion en JavaScript.js
@@ -0,0 +1,29 @@
+//Notacion en JavaScript
+
+//; --> sirve para delimitar el final de cada linea 
+const b = 4;
+
+b+4;
+
+//. --> se utiliza en los objetos para acceder a los atributos
+
+// movil.anchura 
+
+//[] --> se utiliza para las listas arreglos o arrays 
+const ar = [1,2,3,401]
+console.log(ar[2])
+// ()--> se utiliza para las funciones 
+function suma (a,b){
+    // se escribe la funcion
+}
+
+//{}--> se utiliza para objetos funciones y estructuras de control 
+const movil = {
+    anchura: 5,
+    altura: 10
+}
+if (true) {
+    // todo lo que haya entre llaves 
+
+    const constante2 = "hola"
+}
